44e692d2baf0635029a9400183c39b1fe3eba9ed
Liyuanzhe2001 <619356147@qq.com>
2023-03-30 18:01:24 +0800

    'update'

---
+++ 08-结构体/结构体/结构体指针.cpp
@@ -0,0 +1,20 @@
+//#include<iostream>
+//using namespace std;
+//
+////结构体定义
+//struct student
+//{
+//	//成员列表
+//	string name;  //姓名
+//	int age;      //年龄
+//	int score;    //分数
+//};
+//
+//int main() {
+//	student stu = { "张三",18,100 };
+//	student* p = &stu;
+//
+//	p->score = 80; // 指针通过 -> 操作符可以访问成员
+//
+//	cout << "姓名：" << stu.name << " 年龄：" << stu.age << " 分数：" << stu.score << endl;
+//}
---
+++ 11-内存分区模型/内存分区模型/new操作符-基本语法.cpp
@@ -0,0 +1,24 @@
+//#include<iostream>
+//using namespace std;
+//
+//int* func() {
+//	int* a = new int(10);
+//	return a;
+//}
+//
+//int main() {
+//	int* p = func();
+//
+//	cout << *p << endl;
+//	cout << *p << endl;
+//
+//	// 利用 delete释放堆取数据
+//	delete p;
+//
+//	//cout << *p << endl; //报错，释放的空间不可访问
+//
+//	system("pause");
+//
+//	return 0;
+//
+//}
